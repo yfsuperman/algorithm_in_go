a4ef856cd344ed8131e5efd8b7327317b8ddc9ec
yfsuperman <yf6720wly@gmail.com>
2018-12-03 23:12:12 -0500

    add go code for First-Missing-Positive

---
+++ GO/First-Missing-Positive/firstMissingPositive.go
@@ -0,0 +1,37 @@
+package firstMissingPositive
+
+// Given an unsorted integer array, find the smallest missing positive integer.
+
+// Example 1:
+
+// Input: [1,2,0]
+// Output: 3
+// Example 2:
+
+// Input: [3,4,-1,1]
+// Output: 2
+// Example 3:
+
+// Input: [7,8,9,11,12]
+// Output: 1
+// Note:
+
+// Your algorithm should run in O(n) time and uses constant extra space.
+
+func firstMissingPositive(nums []int) int {
+    n := len(nums)
+    
+    for i := range nums {
+        for nums[i] >= 1 && nums[i] <= n && nums[nums[i]-1] != nums[i] {
+            nums[i], nums[nums[i]-1] = nums[nums[i]-1], nums[i]
+        }
+    }
+
+    for i, v := range nums {
+        if v != i + 1 {
+            return i + 1
+        }
+    }
+    
+    return n+1
+}
